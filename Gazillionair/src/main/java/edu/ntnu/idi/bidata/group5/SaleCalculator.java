61c2dbdb64707a95e56d27c6c295405087b0d405
Viet-Uy Do <vietuydo@gmail.com>
2026-02-05 15:46:59 +0100

    added Transaction, purchase and sale calc

--- Gazillionair/src/main/java/edu/ntnu/idi/bidata/group5/SaleCalculator.java
+++ Gazillionair/src/main/java/edu/ntnu/idi/bidata/group5/SaleCalculator.java
@@ -1,4 +1,35 @@
 package edu.ntnu.idi.bidata.group5;
 
-public class SaleCalculator {
+import java.math.BigDecimal;
+
+abstract class SaleCalculator implements TransactionCalculator {
+
+  private final Share share;
+  BigDecimal salesPrice;
+  BigDecimal quantity;
+
+  public SaleCalculator(Share share) {
+    this.share = share;
+  }
+
+  @Override
+  public BigDecimal calculateGross() {
+    return salesPrice.multiply(quantity);
+  }
+
+  @Override
+  public BigDecimal calculateCommission() {
+    return calculateGross().multiply(BigDecimal.valueOf(0.01));
+  }
+
+  @Override
+  public BigDecimal calculateTax() {
+    BigDecimal purchaseCost = share.purchasePrice.multiply(quantity);
+    return calculateGross().subtract(calculateCommission().subtract(purchaseCost));
+  }
+
+  @Override
+  public BigDecimal calculateTotal() {
+    return calculateGross().subtract(calculateCommission()).subtract(calculateTax());
+  }
 }
--- Gazillionair/src/main/java/edu/ntnu/idi/bidata/group5/TransactionCalculator.java
+++ Gazillionair/src/main/java/edu/ntnu/idi/bidata/group5/TransactionCalculator.java
@@ -1,4 +1,40 @@
 package edu.ntnu.idi.bidata.group5;
 
+import java.math.BigDecimal;
+
+/**
+ * Defining methods that are common in the buy and sell area of the program.
+ */
 public interface TransactionCalculator {
+
+  /**
+   * Calculates the gross value of the transaction before fees and taxes.
+   *
+   * @return the gross value of the transaction before fees and taxes.
+   */
+  BigDecimal calculateGross();
+
+  /**
+   * Calculates the commission amount.
+   *
+   * @return the commission amount
+   */
+  BigDecimal calculateCommission();
+
+  /**
+   * Calculates the tax amount applied to the transaction.
+   *
+   * @return the tax amount
+   */
+  BigDecimal calculateTax();
+
+  /**
+   * Calculates the total cost of the transaction including
+   * gross amount, commission, and tax.
+   *
+   * @return the total transaction cost
+   */
+  BigDecimal calculateTotal();
 }
+
+
