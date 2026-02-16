596558f55f04e1be689a0a4379d387d3c6f71158
Viet-Uy Do <vietuydo@gmail.com>
2026-02-16 10:29:20 +0100

    Fixing tests and classes to match criteria

--- Gazillionair/src/test/java/edu/ntnu/idi/bidata/group5/SaleCalculatorTest.java
+++ Gazillionair/src/test/java/edu/ntnu/idi/bidata/group5/SaleCalculatorTest.java
@@ -10,28 +10,28 @@ class SaleCalculatorTest {
     @Test
     void calculateGross() {
         SaleCalculator saleCalculator = new SaleCalculator(new Share(new Stock("AAPL", "Apple", BigDecimal.valueOf(100)), BigDecimal.valueOf(90), BigDecimal.valueOf(10)));
-        BigDecimal expectedGross = BigDecimal.valueOf(1000); // 10 shares * $100 sales price
+        BigDecimal expectedGross = BigDecimal.valueOf(9000); // 90 shares * $100 sales price
         assertEquals(expectedGross, saleCalculator.calculateGross());
     }
 
     @Test
     void calculateCommission() {
         SaleCalculator saleCalculator = new SaleCalculator(new Share(new Stock("AAPL", "Apple", BigDecimal.valueOf(100)), BigDecimal.valueOf(90), BigDecimal.valueOf(10)));
-        BigDecimal expectedCommission = BigDecimal.valueOf(10); // 1% of $1000 gross
-        assertEquals(expectedCommission, saleCalculator.calculateCommission());
+        BigDecimal expectedCommission = BigDecimal.valueOf(90); // 1% of $9000 gross
+        assertEquals(0,expectedCommission.compareTo(saleCalculator.calculateCommission()));
     }
 
     @Test
     void calculateTax() {
         SaleCalculator saleCalculator = new SaleCalculator(new Share(new Stock("AAPL", "Apple", BigDecimal.valueOf(100)), BigDecimal.valueOf(90), BigDecimal.valueOf(10)));
-        BigDecimal expectedTax = BigDecimal.valueOf(100); // ($1000 gross - $10 commission) - ($900 purchase cost)
-        assertEquals(expectedTax, saleCalculator.calculateTax());
+        BigDecimal expectedTax = BigDecimal.valueOf(2403); // 30% of ($9000 gross - $90 commission - $900 purchase cost)
+        assertEquals(0, expectedTax.compareTo(saleCalculator.calculateTax()) );
     }
 
     @Test
     void calculateTotal() {
         SaleCalculator saleCalculator = new SaleCalculator(new Share(new Stock("AAPL", "Apple", BigDecimal.valueOf(100)), BigDecimal.valueOf(90), BigDecimal.valueOf(10)));
-        BigDecimal expectedTotal = BigDecimal.valueOf(890); // $1000 gross - $10 commission - $100 tax
-        assertEquals(expectedTotal, saleCalculator.calculateTotal());
+        BigDecimal expectedTotal = BigDecimal.valueOf(6507); // $9000 gross - $90 commission - $2403 tax
+        assertEquals(0,expectedTotal.compareTo(saleCalculator.calculateTotal()) );
     }
 }
