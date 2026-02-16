019546d8d97d6917701ed4392f6ad66aebf76847
Viet-Uy Do <vietuydo@gmail.com>
2026-02-16 12:51:20 +0100

    TransactionArchive testclass, prepping for exceptionhandling

--- Gazillionair/src/test/java/edu/ntnu/idi/bidata/group5/SaleTest.java
+++ Gazillionair/src/test/java/edu/ntnu/idi/bidata/group5/SaleTest.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idi.bidata.group5;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,14 @@ class SaleTest {
 
     @Test
     void commit() {
+        Stock stock = new Stock("AAPL", "Apple", BigDecimal.valueOf(100));
+        Share share = new Share(stock, BigDecimal.valueOf(90), BigDecimal.valueOf(10));
 
+        Player player = new Player("TestPlayer", BigDecimal.valueOf(10000));
+        player.getPortfolio().addShare(share);
+        Sale sale = new Sale(share, 1);
+        sale.commit(player);
+
+        assertTrue(sale.isCommitted());
     }
 }
