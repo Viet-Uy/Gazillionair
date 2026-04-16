0452ad51bb292bcb29b61b52fcf150a6d9a06be6
Viet-Uy <vietuydo@gmail.com>
2026-04-16 13:33:59 +0200

    Javadoc and logic

--- Gazillionair/src/main/java/edu/ntnu/idi/bidata/group5/main/GazillionairApp.java
+++ Gazillionair/src/main/java/edu/ntnu/idi/bidata/group5/main/GazillionairApp.java
@@ -1,4 +1,10 @@
 package edu.ntnu.idi.bidata.group5.main;
 
+/**
+ * Launch the application, initializes the main components and starts the UI.
+ * This class serves as the entry point for the application,
+ * setting up the necessary components and launching the user interface.
+ * It is responsible for creating instances of the main classes and starting
+ */
 public class GazillionairApp {
 }
--- Gazillionair/src/main/java/edu/ntnu/idi/bidata/group5/model/observer/ObservableModel.java
+++ Gazillionair/src/main/java/edu/ntnu/idi/bidata/group5/model/observer/ObservableModel.java
@@ -1,10 +1,26 @@
 package edu.ntnu.idi.bidata.group5.model.observer;
 
+/**
+ * Contract for models that publish state changes to observers.
+ */
 public interface ObservableModel {
 
+  /**
+   * Adds an observer that should be notified on state changes.
+   *
+   * @param observer observer instance
+   */
   void addObserver(ModelObserver observer);
 
+  /**
+   * Removes a previously registered observer.
+   *
+   * @param observer observer instance
+   */
   void removeObserver(ModelObserver observer);
 
+  /**
+   * Notifies all observers that model state has changed.
+   */
   void notifyObservers();
 }
